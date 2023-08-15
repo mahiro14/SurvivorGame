37a847f2325c8a163c1da1534f2b80ab7d773243
mahiroikuta <ikuta.mahiro@donuts.ne.jp>
2023-08-15 18:07:01 +0900

    update:int

---
+++ SurvivorsLikeGame/Assets/Scripts/GameSceneDirector.cs
@@ -0,0 +1,54 @@
+using System.Runtime.Intrinsics;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneDirector : MonoBehaviour
+{
+    [SerializeField] GameObject grid;
+    [SerializeField] Tilemap tilemapCollider;
+
+    public Vector2 TileMapStart;
+    public Vector2 TileMapEnd;
+    public Vector2 WorldStart;
+    public Vector2 WorldEnd;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform item in grid.GetComponentsInChildren<Transform>())
+        {
+            if(TileMapStart.x > item.position.x)
+            {
+                TileMapStart.x = item.position.x;
+            }
+            if(TileMapStart.y > item.position.y)
+            {
+                TileMapStart.y = item.position.y;
+            }
+
+            if(TileMapEnd.x < item.position.x)
+            {
+                TileMapEnd.x = item.position.x;
+            }
+            if(TileMapEnd.y < item.position.y)
+            {
+                TileMapEnd.y = item.position.y;
+            }
+        }
+
+        float cameraSize = Camera.main.orthographicSize;
+        float aspect = (float)Screen.width / (float)Screen.height;
+        WorldStart = new Vector2(TileMapStart.x - cameraSize * aspect, TileMapStart.y - cameraSize);
+        WorldEnd = new Vector2(TileMapEnd.x + cameraSize * aspect, TileMapEnd.y + cameraSize);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
